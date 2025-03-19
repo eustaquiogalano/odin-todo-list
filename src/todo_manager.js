ab825faadd78443138f4dfd528391b276d035177
eustaquio <eustaquio.galano@gmail.com>
2025-03-19 12:01:16 +0800

    chore: move to designated folder

--- src/todo_manager.js
+++
@@ -1,9 +0,0 @@
-export default class Todo {
-    constructor(title, description, dueDate, priority, checkList) {
-        this.title = title;
-        this.description = description;
-        this.dueDate = dueDate;
-        this.priority = priority;
-        this.checkList = checkList;
-    }
-}
