84047f891f8904962e1e5961b0f89cb39e6e3f63
eustaquio <eustaquio.galano@gmail.com>
2025-03-22 16:48:00 +0800

    feat: convert project class to factory function

--- src/scripts/project_components/project_manager.js
+++ src/scripts/project_components/project_manager.js
@@ -1,25 +1,9 @@
-export default class Project {
-    constructor(projectName) {
-        this.projectName = projectName;
-        this.todoList = [];
-    }
+import { todo } from "./addTodo.js";
 
-    addTodo(todo) {
-        this.todoList.push(todo);
-    }
-
-    createDefaultProject() {
-        // create default project
-        const defaultProject = new Project("Default Project");
-
-        // create default todos 
-        const defaultTodo1 = new Todo("Workout", "Leg day - 100 reps", "12-05-25", 1, false);
-        const defaultTodo2 = new Todo("Bible", "Read and study scriptures", "12-05-25", 1, false);
-
-        // add default todos in default project
-        defaultProject.addTodo(defaultTodo1);
-        defaultProject.addTodo(defaultTodo2);
-
-        return defaultProject;
+export function createProject(projectName) {
+    return {
+        projectName,
+        todoList: [],
+        ...todo
     }
 }
