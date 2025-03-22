6dd5940cc692b63c3337f2310991f1cb71b44a43
eustaquio <eustaquio.galano@gmail.com>
2025-03-22 21:58:48 +0800

    feat: import modules for future use

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import "./styles.css";
-import Todo from "./scripts/todo_manager.js";
-
-const task1 = new Todo("Workout", "Leg day", new Date(), 1, "checked");
-
-console.log(task1);
+import Todo from "./scripts/todo_components/todo_manager.js";
+import { storeProject } from "./scripts/storage_components/storage_manager.js";
+import { createProject } from "./scripts/project_components/project_manager.js";
+import { domRenderer } from "./scripts/dom_components/dom_manager.js";
 
