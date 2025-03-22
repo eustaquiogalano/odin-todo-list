08b41f2381b7d79973eecb5ab66d071730761578
eustaquio <eustaquio.galano@gmail.com>
2025-03-22 21:35:05 +0800

    refactor: convert class to factory function

--- src/scripts/dom_components/dom_manager.js
+++ src/scripts/dom_components/dom_manager.js
@@ -1,14 +1,7 @@
-export default class Render {
+import { renderer } from "./render_projects.js";
 
-    // displays all the project inside local storage
-    renderProjects() {
-        const allProjects = Object.fromEntries(
-            Object.keys(localStorage).map((key) => {
-                return [key, localStorage.getItem(key)];
-            })
-        );
-        // console is for temporary display
-        //  while finishing the code logic 
-        console.log(allProjects);
+export function domRenderer() {
+    return {
+        ...renderer,
     }
 }
