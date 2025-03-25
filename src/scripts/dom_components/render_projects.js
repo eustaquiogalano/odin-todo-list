0902da047560a3d4c71887f64a8cca826a835ac8
eustaquio <eustaquio.galano@gmail.com>
2025-03-25 12:43:48 +0800

    feat: update renderProject function
     - Get a reference of project list for manipulation
     - Concatenate all the project names
     - Display all the project names in project list element

--- src/scripts/dom_components/render_projects.js
+++ src/scripts/dom_components/render_projects.js
@@ -1,14 +1,30 @@
+const projectList = document.querySelector("#project-list");
+
 export const renderer = {
 
     // displays all the project inside local storage
     renderProjects() {
+
+        let generateHTML = '';
+
         const allProjects = Object.fromEntries(
             Object.keys(localStorage).map((key) => {
+
+                // Parse to object the string from local storage
+                let currentProject = JSON.parse(localStorage.getItem(key));
+
+                // Concatenate the projects names enclose with li tag 
+                generateHTML += `<li>${currentProject.projectName}</li>`;
+
+                // return a key value pair array
                 return [key, localStorage.getItem(key)];
             })
         );
-        // console is for temporary display
-        //  while finishing the code logic 
+
+        // Set the combined project names  as the html content of projject list 
+        projectList.innerHTML = generateHTML;
+ 
+        // this console is for debugging purposes only
         console.log(allProjects);
     }
 }
