20b96fadd74f85ed6399f16656376f079a2c5333
eustaquio <eustaquio.galano@gmail.com>
2025-03-29 18:11:41 +0800

    feat: add the function inside arrow function

--- src/scripts/dom_components/init_button.js
+++ src/scripts/dom_components/init_button.js
@@ -1,12 +1,13 @@
 import { renderTodos } from "./render_todos.js";
 
-
 export function initProjectButton() {
     const projectButtonList = document.querySelectorAll(".projectButton");
     console.log(projectButtonList); 
     
     projectButtonList.forEach((button) => {
-        button.addEventListener("click", renderTodos.renderTodosToDom);
+        button.addEventListener("click", () => {
+            renderTodos.renderTodosToDom(button.id);
+        })
     });
 
 }
