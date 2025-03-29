54fd27804e2500a91a042344839b3612e930023b
eustaquio <eustaquio.galano@gmail.com>
2025-03-29 18:19:08 +0800

    feat: modify render todos to DOM functinality
    - Module modified
    - Todos that will appear will be based on what the user clicked in
        project button list

--- src/scripts/dom_components/render_todos.js
+++ src/scripts/dom_components/render_todos.js
@@ -1,52 +1,42 @@
 const todoListContainer = document.querySelector("#todo-list-container");
 
 export const renderTodos = {
-    renderTodosToDom() {
+    renderTodosToDom(id) {
 
+        // get the clicked button
+        // arrange to be used as key 
+        // local storage
+        const projectKey = id.split("-").join(" ");
+
+        // initialize empty array 
         let generateHTML = '';
 
-        const allProjects = Object.fromEntries(
-            Object.keys(localStorage).map((key) => {
+        // get the todo list of the selected project 
+        // based on the clicked button
+        const todoList = JSON.parse(localStorage.getItem(projectKey)).todoList;
 
-                // Parse to object the string from local storage
-                let currentProject = JSON.parse(localStorage.getItem(key));
+        // loop through the todo list 
+        // concatenate all generated html for each todo
+        todoList.forEach(todo => {
+            generateHTML += `
+                <div class="todo-container">
+                    <div class="container-left-side">
+                        <p class="todo-priority">${todo.priority}</p>
+                        <button class="checkbox"></button>
+                    </div>
+                    <div class="container-right-side">
+                        <h3 class="todo-title">${todo.title}</h3>
+                        <p class="todo-due-date">${todo.dueDate}</p>
+                        <p class="todo-description">
+                            ${todo.description}
+                        </p>
+                    </div>
+                </div>
+            `;
+        });
 
-                // Loop through the todo list of this
-                // current project then generate html 
-                // for each todo for display purposes
-                let todos = currentProject.todoList;
-
-                for (let i = 0; i < todos.length; i++) {
-                    let todo = todos[i]
-                    generateHTML += `
-                
-                        <div class="todo-container">
-                            <div class="container-left-side">
-                                <p class="todo-priority">${todo.priority}</p>
-                                <button class="checkbox"></button>
-                            </div>
-                            <div class="container-right-side">
-                                <h3 class="todo-title">${todo.title}</h3>
-                                <p class="todo-due-date">${todo.dueDate}</p>
-                                <p class="todo-description">
-                                    ${todo.description}
-                                </p>
-                            </div>
-                        </div>
-                
-                    `;
-                }
-               
-
-                // return a key value pair array
-                return [key, localStorage.getItem(key)];
-            })
-        );
-
-        // Set the combined project names  as the html content of projject list 
+        // use the generated html as the content for 
+        // todolist container
         todoListContainer.innerHTML = generateHTML;
-
-        // this console is for debugging purposes only
-        console.log(allProjects)
     }
 }
