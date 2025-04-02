3640af77c4fff55c6bcff0e1b688cf2127c8493e
eustaquio <eustaquio.galano@gmail.com>
2025-04-02 16:07:28 +0800

    feat: add module for handlign all event listeners

---
+++ src/event_listeners.js
@@ -0,0 +1,26 @@
+// import { createProjectModal } from "./scripts/project_components/modals.js";
+
+export function initializeModals() {
+
+    const addProjectModal = document.querySelector("#add-project-modal");
+
+    // modal for adding project will apear
+    document.querySelector(".open-modal").addEventListener("click", () => {
+        addProjectModal.style.display = "flex";
+    });
+
+     // exit modal 
+     document.querySelector(".close-modal").addEventListener("click", () => {
+        addProjectModal.style.display = "none";
+    });
+
+}
+
+export function initializeProjectCreation() {
+
+    document.querySelector("#add-to-list").addEventListener("click", () => {
+        console.log("asdfasdf"); 
+        
+    });
+
+}
