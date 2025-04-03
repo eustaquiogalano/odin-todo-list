71988dba44caac1a7c87fdca38f3afe9df4203aa
eustaquio <eustaquio.galano@gmail.com>
2025-04-03 21:34:42 +0800

    feat: add imports and initializations
    - add imports needed for event listeners to work
    - add functionality for "add project" button

--- src/scripts/event_listeners.js
+++ src/scripts/event_listeners.js
@@ -1,16 +1,26 @@
+import { projectStorageAccess } from "./storage_components/storage_manager";
+import { createProject } from "./project_components/project_manager";
+import { domRenderer } from "./dom_components/dom_manager";
+
+// Will be used for both initializer functions
+// initialiazeModals()
+// initializeProjectCreation()
+const projectName = document.querySelector("#project-name-input");
+
+// get a reference of the modal
+const addProjectModal = document.querySelector("#add-project-modal");
 
 export function initializeModals() {
-
-    const addProjectModal = document.querySelector("#add-project-modal");
 
     // modal for adding project will apear
     document.querySelector(".open-modal").addEventListener("click", () => {
         addProjectModal.style.display = "flex";
     });
 
-     // exit modal 
-     document.querySelector(".close-modal").addEventListener("click", () => {
+    // for exiting modal 
+    document.querySelector(".close-modal").addEventListener("click", () => {
         addProjectModal.style.display = "none";
+        projectName.value = "";
     });
 
 }
@@ -18,8 +28,34 @@ export function initializeModals() {
 export function initializeProjectCreation() {
 
     document.querySelector("#add-to-list").addEventListener("click", () => {
-        
-        
+
+        // create new project
+        // get the name from the input from modal 
+        const newProject = createProject(projectName.value);
+
+        // create new storage object for 
+        // storing and retreiving projects from
+        // the local storage
+        const storage = projectStorageAccess();
+
+        // store the newly created project
+        storage.storeProject(newProject.projectName, newProject); 
+
+        // create new renderer object for 
+        // rendering purposes
+        const renderer = domRenderer();
+
+        // render projects to the DOM
+        renderer.renderProjectsToDom();
+
+        // initialize project buttons
+        renderer.initProjectButton();
+
+        // exit the modal after click event
+        // reset the value of the input
+        addProjectModal.style.display = "none";
+        projectName.value = ""; 
+
     });
 
 }
