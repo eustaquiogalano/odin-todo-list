73fa93a11aff7b40f9d0f2a3079aa693aabe1577
eustaquio <eustaquio.galano@gmail.com>
2025-04-05 11:49:44 +0800

    refactor: remove hard coded elements

--- src/scripts/dom_components/render_project_name.js
+++ src/scripts/dom_components/render_project_name.js
@@ -1,16 +1,9 @@
-const projectDetailsContainer = document.querySelector("#project-details-container");
+const leftSide = document.querySelector("#left-side");
 
 export function renderProjectName(id, projectKey) {
 
-    projectDetailsContainer.innerHTML = `
-    
-        <div id="left-side">
-            <h3 id="${id}-title">${projectKey}</h3>
-            <p id="creation-date">05-05-25</p>
-        </div>
-        <div id="right-side">
-            <button class="open-add-todo-modal">Add todo</button>
-        </div>
-
+    leftSide.innerHTML = `
+        <h3 id="${id}-title">${projectKey}</h3>
+        <p id="creation-date">05-05-25</p>
     `;
 }
