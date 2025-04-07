2c9f6c161012c1f3e4c00442ed65e080be80ac13
eustaquio <eustaquio.galano@gmail.com>
2025-04-07 14:58:18 +0800

    feat: add display functionality of add todo button

--- src/scripts/dom_components/render_project_name.js
+++ src/scripts/dom_components/render_project_name.js
@@ -1,4 +1,5 @@
 const leftSide = document.querySelector("#left-side");
+const addTodoButton = document.querySelector("#open-todo-creation-modal");
 
 export function renderProjectName(id, projectKey) {
 
@@ -6,4 +7,6 @@ export function renderProjectName(id, projectKey) {
         <h3 id="${id}-title">${projectKey}</h3>
         <p id="creation-date">05-05-25</p>
     `;
+
+    addTodoButton.style.display = "block";
 }
