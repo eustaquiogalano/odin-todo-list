a6a3f3edb0b2c0e6251e52800b0f66ccb6570444
eustaquio <eustaquio.galano@gmail.com>
2025-04-07 10:25:11 +0800

    feat: add todo creation functionality
    - add imports from other modules
    - make storage module global
    - make the reference of todo creation modal become global
    - rename function
    - add todo creation functionality inside event listener of add todo
        button

--- src/scripts/event_listeners.js
+++ src/scripts/event_listeners.js
@@ -1,6 +1,9 @@
 import { projectStorageAccess } from "./storage_components/storage_manager";
 import { createProject } from "./project_components/project_manager";
 import { domRenderer } from "./dom_components/dom_manager";
+import Todo from "./todo_components/todo_manager";
+import { renderTodos } from "./dom_components/render_todos";
+import { initProjectButton } from "./dom_components/init_button";
 
 // Will be used for both initializer functions
 // initialiazeModals()
@@ -27,6 +30,11 @@ export function initializeModals() {
 
 }
 
+// create new storage object for 
+// storing and retreiving projects from
+// the local storage
+const storage = projectStorageAccess();
+
 // event listener for project creation
 export function initializeProjectCreation() {
 
@@ -35,11 +43,6 @@ export function initializeProjectCreation() {
         // create new project
         // get the name from the input from modal 
         const newProject = createProject(projectName.value);
-
-        // create new storage object for 
-        // storing and retreiving projects from
-        // the local storage
-        const storage = projectStorageAccess();
 
         // store the newly created project
         storage.storeProject(newProject.projectName, newProject);
@@ -63,12 +66,13 @@ export function initializeProjectCreation() {
 
 }
 
+const todoCreationModal = document.querySelector("#todo-creation-modal");
+
 // event listeners for todo creation
-export function initializeTodoCreation() {
+export function initializeTodoCreationModal() {
 
     // references to the element from the DOM
     const openTodoCreationModal = document.querySelector("#open-todo-creation-modal");
-    const todoCreationModal = document.querySelector("#todo-creation-modal");
     const closeTodoCreationModal = document.querySelector(".close-todo-modal");
 
     // This opens the todo creation modal
@@ -79,5 +83,49 @@ export function initializeTodoCreation() {
     // this closes the todo creation modal
     closeTodoCreationModal.addEventListener("click", () => {
         todoCreationModal.style.display = "none";
+    });
+}
+
+export function initializeTodoCreation() {
+
+    document.querySelector("#add-todo-button").addEventListener("click", () => {
+        const projectTitle = document.querySelector("#project-title");
+        const projectDescription = document.querySelector("#project-description");
+        const projectDue = document.querySelector("#project-due");
+        const projectPriority = document.querySelector("#project-priority");
+        const projectName = document.querySelector("#left-side > h3");
+
+        const todo = new Todo(
+            projectTitle.value,
+            projectDescription.value,
+            projectDue.value,
+            projectPriority.value
+        );
+
+        const selectedProject = storage.retreiveProject(projectName.textContent);
+        selectedProject.todoList.push(todo);
+        console.log(selectedProject);
+
+        storage.storeProject(projectName.textContent, selectedProject);
+
+        console.log(projectName.id.split("-").join(" "));
+        
+        renderTodos.renderTodosToDom(projectName.textContent);
+        // initProjectButton();
+
+        
+
+
+
+
+
+
+        projectTitle.value = "";
+        projectDescription.value = "";
+        projectDue.value = "";
+        projectPriority.value = "";
+
+        todoCreationModal.style.display = "none";
+
     });
 }
