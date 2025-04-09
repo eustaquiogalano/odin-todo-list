1b0d75e2fba24f52f65c14d9e0b2e153b79c867b
eustaquio <eustaquio.galano@gmail.com>
2025-04-09 12:04:40 +0800

    feat: add event listener to "edit todo" button
    - comments were also added for this module

--- src/scripts/dom_components/init_button.js
+++ src/scripts/dom_components/init_button.js
@@ -1,12 +1,31 @@
 import { renderTodos } from "./render_todos.js";
 
 export function initProjectButton() {
+
+    // Get a reference of all the project button
     const projectButtonList = document.querySelectorAll(".projectButton");
-    console.log(projectButtonList); 
-    
+
+    // Loop through the list of project button
     projectButtonList.forEach((button) => {
+        
+        // put an event listener
         button.addEventListener("click", () => {
+
+            // Render todo of the current project selected
             renderTodos.renderTodosToDom(button.id);
+
+            // get a reference of edit todo button
+            // for opening the edit modal
+            const editButton = document.querySelector("#edit-todo-button");
+            const todoEditModal = document.querySelector("#expand-todo");
+
+            // put an event listener to the edit button
+            editButton.addEventListener("click", () => {
+
+                // change the display property to display to appear
+                todoEditModal.style.display = "flex";
+
+            });
         })
     });
 
