16c95e54c899e30f1400e7157b00e80de2aa9d44
eustaquio <eustaquio.galano@gmail.com>
2025-04-12 12:31:38 +0800

    feat: add all initializers to index

--- src/index.js
+++ src/index.js
@@ -1,24 +1,50 @@
 import "./styles.css";
 import Todo from "./scripts/todo_components/todo_manager.js";
-import { storeProject } from "./scripts/storage_components/storage_manager.js";
+import { projectStorageAccess } from "./scripts/storage_components/storage_manager.js";
 import { createProject } from "./scripts/project_components/project_manager.js";
 import { domRenderer } from "./scripts/dom_components/dom_manager.js";
+import { initializeModals, initializeProjectCreation, initializeTodoCreation, initializeTodoCreationModal } from "./scripts/event_listeners.js";
+import { initialieDeleteTodoButton } from "./scripts/todo_components/delete_todo.js";
 
 // create instance of storage
-const storage = storeProject();
+const storage = projectStorageAccess();
 
 // create default project and todos
-const defaultProject = createProject("defaultProject");
-const pray = new Todo("Pray", "study", "05-05-25", 1, false);
-const study = new Todo("WD", "study", "05-05-25", 1, false);
+const defaultProject = createProject("Default Project");
+const pray = new Todo("Pray", "study the bible", "05-13-25", 1, false);
+const study = new Todo("WD", "study Web Development", "08-013-25", 2, false);
 defaultProject.addTodo(pray);
 defaultProject.addTodo(study);
 
 // store the default project to local storage
 storage.storeProject(defaultProject.projectName, defaultProject);
 
-// render all project inside local storage
+// initially render all project inside local storage
 const allProjects = domRenderer();
-allProjects.renderProjects();
+allProjects.renderProjectsToDom();
+
+// after html contents was fully loaded
+// invoke these function to initialize
+document.addEventListener("DOMContentLoaded", () => {
+
+    // intialize event listeners on buttons
+    allProjects.initProjectButton();
+
+    // initialize modals open and close buttons
+    initializeModals();
+
+    // initialize project addition button
+    initializeProjectCreation();
+
+    // initialize todo creation button
+    initializeTodoCreationModal();
+
+    // initialize todo creation and adding to the project
+    initializeTodoCreation();
+
+    // initialize delete todo buttons
+    initialieDeleteTodoButton();
+
+});
 
 
